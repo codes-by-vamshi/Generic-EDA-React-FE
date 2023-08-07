96e4c2369e1420b6cd98840967bee295bc76f6f2
codes-by-vamshi <krishnavamshidevarasetty@gmail.com>
2023-08-08 00:52:14 +0530

    Made local and server endpoints for easy testing

--- src/Api/getInfo.js
+++ src/Api/getInfo.js
@@ -1,6 +1,7 @@
+import { API_ENDPOINT } from "./ApiConstants";
 import { _post } from "./commonApi";
 export const getInfo = async (uniqueCode, colNamesIncluded) => {
-    const resp = await _post('https://dskvamshi.pythonanywhere.com/api/getInfo', {}, {
+    const resp = await _post(`${API_ENDPOINT}/api/getInfo`, {}, {
         'Content-Type': 'application/json',
         'Unique-Identity': uniqueCode,
         'Col-Names-Included': colNamesIncluded,
--- src/Api/uploadCsv.js
+++ src/Api/uploadCsv.js
@@ -1,8 +1,9 @@
+import { API_ENDPOINT } from "./ApiConstants";
 import { _post } from "./commonApi";
 export const uploadCsvFile = async (file, uniqueCode, colNamesIncluded) => {
     const formData = new FormData();
     formData.append('file', file);
-    const resp = await _post('https://dskvamshi.pythonanywhere.com/api/uploadCsv', formData, {
+    const resp = await _post(`${API_ENDPOINT}/api/uploadCsv`, formData, {
         'Content-Type': 'multipart/form-data',
         'Unique-Identity': uniqueCode,
         'Col-Names-Included': colNamesIncluded,
